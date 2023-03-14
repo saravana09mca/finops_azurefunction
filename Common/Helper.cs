d0a22451436e565aab3dfb35dc0dcb9108dfee91
saravanan <saravana09mca@gmail.com>
2023-03-15 04:51:00 +0530

    GCP changes

---
+++ Common/Helper.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Budget.TimerFunction
+{
+    public class Helper
+    {
+        public static Stream GetBlobMemoryStream(string storageAccountConnectionString,string containerName,string blobName)
+        {
+
+            var memoryStream = new MemoryStream();         
+            var storageAccount = CloudStorageAccount.Parse(storageAccountConnectionString);
+            var blobClient = storageAccount.CreateCloudBlobClient();
+            var container = blobClient.GetContainerReference(containerName);
+            var blob = container.GetBlockBlobReference(blobName);
+            blob.DownloadToStream(memoryStream);
+            memoryStream.Position = 0;
+            return memoryStream;
+        }
+        public static decimal ValidateDecimal(string input)
+        {
+            decimal number;
+            if (!Decimal.TryParse(input, out number))
+            {
+                number = Decimal.Parse(input, NumberStyles.AllowExponent | NumberStyles.AllowDecimalPoint);
+            }
+            return number;
+        }
+    }
+}
---
+++ Model/GCPAdvisorModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Budget.TimerFunction
+{
+
+    public class GCPAdvisorModel
+    {
+        public string cloud_entity_type { get; set; }
+        public string cloud_entity_id { get; set; }
+        public string name { get; set; }
+        public string location { get; set; }
+        public string recommender { get; set; }
+        public string recommender_subtype { get; set; }
+        //public List<string> target_resources { get; set; }
+        public string description { get; set; }
+        public DateTime last_refresh_time { get; set; }
+        public PrimaryImpact primary_impact { get; set; }
+        //public string state { get; set; }
+        //public Ancestors ancestors { get; set; }
+        //public List<string> associated_insights { get; set; }
+        //public string recommendation_details { get; set; }
+        public string priority { get; set; }
+    }
+    public class Ancestors
+    {
+        public string organization_id { get; set; }
+        public List<string> folder_ids { get; set; }
+    }
+
+    public class Cost
+    {
+        public string currency_code { get; set; }
+        public int? units { get; set; }
+        public int nanos { get; set; }
+    }
+
+    public class CostProjection
+    {
+        public Cost cost { get; set; }
+        public Duration duration { get; set; }
+    }
+
+    public class Duration
+    {
+        public int seconds { get; set; }
+       //public object nanos { get; set; }
+    }
+
+    public class PrimaryImpact
+    {
+        public string category { get; set; }
+        public CostProjection cost_projection { get; set; }
+    } 
+
+
+}
