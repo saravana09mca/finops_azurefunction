86f4956822705d3159a15423eee1d6c90484f2b1
saravanan <saravana09mca@gmail.com>
2023-03-27 12:27:32 +0530

    AWStags and gcpbudget code changes

--- AWSTagDetailsFunction.cs
+++ AWSTagDetailsFunction.cs
@@ -40,6 +40,7 @@ namespace Budget.TimerFunction
             sourceData.Columns.Add("ServiceName");
             sourceData.Columns.Add("ResourceID");
             sourceData.Columns.Add("Tags");
+            sourceData.Columns.Add("CreateDate");
             try
             {
                 foreach (S3Object obj in res.S3Objects)
@@ -56,62 +57,63 @@ namespace Budget.TimerFunction
                             //Add old object to deleteObject List
                             deleteObjReqlist.S3Objects.Add(obj);
                         }
-                    } 
+                    }
                 }
-                        //Select the latest Object based on LastModified
-                        S3Object LatestObject = LatestObjReqlist.S3Objects.OrderBy(a => a.LastModified).LastOrDefault();
-                        if (LatestObject != null)
-                        {
-                            //Extract the Data from the CSV file
-                            var response = s3Client.GetObjectAsync(ConfigStore.Aws.BucketName, LatestObject.Key).Result;
-                            using StreamReader reader = new StreamReader(response.ResponseStream);
-                            using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
-                            using var dr = new CsvDataReader(csv);
+                //Select the latest Object based on LastModified
+                S3Object LatestObject = LatestObjReqlist.S3Objects.OrderBy(a => a.LastModified).LastOrDefault();
+                if (LatestObject != null)
+                {
+                    //Extract the Data from the CSV file
+                    var response = s3Client.GetObjectAsync(ConfigStore.Aws.BucketName, LatestObject.Key).Result;
+                    using StreamReader reader = new StreamReader(response.ResponseStream);
+                    using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+                    using var dr = new CsvDataReader(csv);
 
-                            while (dr.Read())
-                            {
-                                DataRow row = sourceData.NewRow();
-                                row["ServiceCategory"] = dr["ServiceCategory"];
-                                row["ServiceName"] = dr["ServiceName"];
-                                row["ResourceID"] = dr["ResourceID"];
-                                row["Tags"] = dr["Tags"];
-                                sourceData.Rows.Add(row);
-                            }
-                        }
-                        if (sourceData.Rows.Count > 0)
+                    while (dr.Read())
+                    {
+                        DataRow row = sourceData.NewRow();
+                        row["ServiceCategory"] = dr["ServiceCategory"];
+                        row["ServiceName"] = dr["ServiceName"];
+                        row["ResourceID"] = dr["ResourceID"];
+                        row["Tags"] = dr["Tags"];
+                        row["CreateDate"] = DateTime.UtcNow;
+                        sourceData.Rows.Add(row);
+                    }
+                }
+                if (sourceData.Rows.Count > 0)
+                {
+                    using (SqlConnection sourceConnection = new SqlConnection(ConfigStore.SQLConnectionString))
+                    {
+                        sourceConnection.Open();
+                        // Perform an Delete operation for old data from the source table.
+                        SqlCommand commandRowCount = new SqlCommand("Truncate table  " + "dbo.AWSTagDetails;", sourceConnection);
+                        long countStart = System.Convert.ToInt32(commandRowCount.ExecuteScalar());
+                        if (countStart == 0)
                         {
-                            using (SqlConnection sourceConnection = new SqlConnection(ConfigStore.SQLConnectionString))
-                            {
-                                sourceConnection.Open();
-                                // Perform an Delete operation for old data from the source table.
-                                SqlCommand commandRowCount = new SqlCommand("Truncate table  " + "dbo.AWSTagDetails;", sourceConnection);
-                                long countStart = System.Convert.ToInt32(commandRowCount.ExecuteScalar());
-                                if (countStart == 0)
-                                {
-                                    //Perform Bulk Insert Opertion to Source table
-                                    SqlBulkCopy bcp = new SqlBulkCopy(ConfigStore.SQLConnectionString);
-                                    bcp.DestinationTableName = "AWSTagDetails";
-                                    bcp.WriteToServer(sourceData);
-                                    IsBulkInsertResult = true;
-                                }
-                                sourceConnection.Close();
-                            }
+                            //Perform Bulk Insert Opertion to Source table
+                            SqlBulkCopy bcp = new SqlBulkCopy(ConfigStore.SQLConnectionString);
+                            bcp.DestinationTableName = "AWSTagDetails";
+                            bcp.WriteToServer(sourceData);
+                            IsBulkInsertResult = true;
                         }
-                        //After succesful bulk insert delete old files from s3 bucket
-                        if (IsBulkInsertResult == true && deleteObjReqlist.S3Objects.Count > 0)
+                        sourceConnection.Close();
+                    }
+                }
+                //After succesful bulk insert delete old files from s3 bucket
+                if (IsBulkInsertResult == true && deleteObjReqlist.S3Objects.Count > 0)
+                {
+                    foreach (S3Object oldObj in deleteObjReqlist.S3Objects)
+                    {
+                        //Add Key name and Bucket name in deleteObjectRequest
+                        DeleteObjectRequest deleteObjReq = new DeleteObjectRequest
                         {
-                            foreach (S3Object oldObj in deleteObjReqlist.S3Objects)
-                            {
-                                //Add Key name and Bucket name in deleteObjectRequest
-                                DeleteObjectRequest deleteObjReq = new DeleteObjectRequest
-                                {
-                                    BucketName = oldObj.BucketName,
-                                    Key = oldObj.Key
-                                };
-                                //Perform Delete operation 
-                                await s3Client.DeleteObjectAsync(deleteObjReq);
-                            }
-                        }
+                            BucketName = oldObj.BucketName,
+                            Key = oldObj.Key
+                        };
+                        //Perform Delete operation 
+                        await s3Client.DeleteObjectAsync(deleteObjReq);
+                    }
+                }
             }
 
             catch (Exception Excep)
