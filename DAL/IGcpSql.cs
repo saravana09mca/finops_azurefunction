3b75b54191e1426ab5276dc02a52b04b507435bf
saravanan <saravana09mca@gmail.com>
2023-04-07 10:58:17 +0530

    GCP code refactoring

---
+++ DAL/IGcpSql.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+using Microsoft.Extensions.Logging;
+
+
+
+namespace Budget.TimerFunction
+{
+   public interface IGcpSql
+    {
+        void SaveGcpAdvisor(List<GCPAdvisorModel.GCPAdvisor> objAdvisor);
+        void SaveBillingCost(List<GCPBillingCostModel.GCPBillingCost> listGCPDdata);
+        void SaveGcpBudget(List<GcpBudgetModel.GcpBudget> objBudgetList, string date);
+        void SaveCarbonFootPrint(DataTable dt, string date);
+        void SaveGcpTags(List<GcpTagsModel.GcpTags> listGCPTagsdata);
+        void SaveGcpUtilization(List<GCPUtilizationModel.GCPUtilization> objUtilization, string date);
+        void SaveGcpOrphaned(List<GCPAdvisorModel.GCPAdvisor> objAdvisor);
+    }
+}
--- TimerFunctions/Gcp/TimerTrigger_GcpCarbon.cs
+++ TimerFunctions/Gcp/TimerTrigger_GcpCarbon.cs
@@ -12,11 +12,20 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using System.Linq;
 using System.Data;
+using AzureFunction.Services.Gcp;
 
 namespace Budget.TimerFunction.Gcp
 {
-    public class GcpCarbon
+    public class TimerTrigger_GcpCarbon
     {
+        private readonly IGcpCarbon _gcpCarbon;
+        private readonly IGcpCredentials _gcpCredentials;
+        public TimerTrigger_GcpCarbon(IGcpCarbon gcpCarbon,
+            IGcpCredentials gcpCredentials)
+        {
+            _gcpCarbon = gcpCarbon;
+            _gcpCredentials = gcpCredentials;   
+        }
         [FunctionName("TimerTrigger_GcpCarbon")]
         public void Run([TimerTrigger("%GCP_CarbonTimer%")] TimerInfo myTimer, ILogger log)
         {
@@ -24,92 +33,17 @@ namespace Budget.TimerFunction.Gcp
             {
                 log.LogInformation($"GCP Carbon Foot Print function executed at: {DateTime.Now}");
 
-                List<GcpBudgetModel.GcpBudget> objBudgetList = new List<GcpBudgetModel.GcpBudget>();
+                var client = _gcpCredentials.GcpAuthentication();
 
+                _gcpCarbon.PutGcpCarbon(client);
 
-                GoogleCredential credentials = null;
-
-                using (var stream = Helper.GetBlobMemoryStream(ConfigStore.AzureStorageAccountConnectionString, ConfigStore.GCP.GCP_BlobContrainerName, ConfigStore.GCP.GCP_BlobFileName))
-                {
-                    credentials = GoogleCredential.FromStream(stream);
-                }
-
-                var client = BigQueryClient.Create(ConfigStore.GCP.GCP_ProjectId, credentials);
-                DateTime datetime = DateTime.UtcNow;
-                var date = new DateTime(datetime.Year, datetime.Month, 1);
-                //var endDate = date.AddMonths(1).AddDays(-1);
-
-                log.LogInformation($"GCP Carbon Foot Print Data Date {date.ToString("yyyy-MM-dd")}");
-
-                DataTable dt= GetGcpCarbonFootPrintList(client, date.ToString("yyyy-MM-dd"), log);
-
-                GcptoSql.SaveCarbonFootPrint(dt, date.ToString("yyyy-MM-dd"), log);
+                log.LogInformation($"GCP Carbon function Process Completed..");
             }
             catch (Exception ex)
             {
-                log.LogError(ex, ex.Message);
-                throw ex;
+                log.LogError(ex, $"Gcp Carbon error occurred in the Timer Trigger Function - {ex.Message}");
+                throw new Exception($"Gcp Carbon Error - {ex.Message}", ex);
             }
-        }
-        public DataTable GetGcpCarbonFootPrintList(BigQueryClient client, string date, ILogger log)
-        {
-            var query = $"SELECT usage_month as UsageMonth," +
-                $"project.number as ProjectNumber," +
-                $"project.id as ProjectId," +
-                $"service.id as ServiceId," +
-                $"service.description as ServiceDesc," +
-                $"location.location as Location," +
-                $"location.region as Region," +
-                $"carbon_footprint_kgCO2e.scope1 as Scope1," +
-                $"carbon_footprint_kgCO2e.scope2.location_based as Scope2," +
-                $"carbon_footprint_kgCO2e.scope3 as Scope3," +
-                $"carbon_footprint_total_kgCO2e.location_based as TotalKgCO2eLocationBased," +
-                $"(carbon_footprint_kgCO2e.scope1 + carbon_footprint_kgCO2e.scope2.location_based + carbon_footprint_kgCO2e.scope3 + carbon_footprint_total_kgCO2e.location_based) as TotalCarbonFootPrint " +                
-                $"FROM `eygds-sandbox-cloud-359111.billing_info_1.carbon_footprint` " +
-                $"where Date(usage_month)>='{date}'";
-
-            log.LogInformation($"GCP Budget query '{query}'");
-
-            // Run the query and get the results
-            var results = client.ExecuteQuery(query, parameters: null);
-            log.LogInformation($"No of GCP Budget rows {results.TotalRows} returned");
-
-            DataTable dt = new DataTable();
-            dt.Columns.Add("Id");
-            dt.Columns.Add("UsageMonth");
-            dt.Columns.Add("ProjectNumber");
-            dt.Columns.Add("ProjectId");
-            dt.Columns.Add("ServiceId");
-            dt.Columns.Add("ServiceDesc");
-            dt.Columns.Add("Location");
-            dt.Columns.Add("Region");
-            dt.Columns.Add("Scope1");
-            dt.Columns.Add("Scope2");
-            dt.Columns.Add("Scope3");
-            dt.Columns.Add("TotalKgCO2eLocationBased");
-            dt.Columns.Add("TotalCarbonFootPrint");
-            dt.Columns.Add("CreatedOn");          
-           
-            foreach (var data in results)
-            {
-                DataRow row = dt.NewRow();
-                row["Id"] = null;
-                row["UsageMonth"] = data["UsageMonth"];
-                row["ProjectNumber"] = data["ProjectNumber"];
-                row["ProjectId"] = data["ProjectId"];
-                row["ServiceId"] = data["ServiceId"];
-                row["ServiceDesc"] = data["ServiceDesc"];
-                row["Location"] = data["Location"];
-                row["Region"] = data["Region"];
-                row["Scope1"] = Helper.ValidateDecimal(Convert.ToString(data["Scope1"]));
-                row["Scope2"] = Helper.ValidateDecimal(Convert.ToString(data["Scope2"]));
-                row["Scope3"] = Helper.ValidateDecimal(Convert.ToString(data["Scope3"]));
-                row["TotalKgCO2eLocationBased"] = Helper.ValidateDecimal(Convert.ToString(data["TotalKgCO2eLocationBased"]));
-                row["TotalCarbonFootPrint"] = Helper.ValidateDecimal(Convert.ToString(data["TotalCarbonFootPrint"]));
-                row["CreatedOn"] = DateTime.UtcNow;
-                dt.Rows.Add(row);
-            }
-            return dt;
         }
        
     }
